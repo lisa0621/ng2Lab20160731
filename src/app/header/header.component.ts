1dd1a371c9833418c875a1d981fa55881c9607cf
SHA-PAI LI <SHA-PAI LI>
2016-07-31 12:31:24 +0800

    input enter event change title

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
     title = 'The Will Will Web';
+    editortitle = this.title;
     titlelink = "http://blog.miniasp.com/";
     num = 1;
   constructor() {
@@ -19,5 +20,14 @@ export class HeaderComponent implements OnInit {
 
   PlusOne() {
     this.num = this.num + 1;
+  }
+
+  eventHandler(event) {
+   //console.log(event, event.keyCode, event.keyIdentifier);
+   if(event.keyCode == 13)
+   {
+      console.log(this.editortitle);
+      this.title = this.editortitle;
+   }
   }
 }
